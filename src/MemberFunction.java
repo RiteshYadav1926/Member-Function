10a52c4d2249f4002acc70d384047d1541cf9f49
RiteshYadav1926 <ritesh192624@gmail.com>
2025-07-09 21:09:01 +0530

    Update MemberFunction.java
    Ab isme Jaana
    Jo new method humne bnaya tha 'CalculateScore' wala
    
    Uska Use karke
    Jo 'main' Method(Member Function) tha...... uska Code Simplify
    Easy and Sundar bnaa liya

--- src/MemberFunction.java
+++ src/MemberFunction.java
@@ -1,30 +1,18 @@
 public class MemberFunction {
         public static void main(String[] args) {
 
-            boolean GameOver = true;
-            int Score = 800;
-            int LevelCompleted = 5;
-            int Bonus = 100;
 
-            int FinalScore = Score + (LevelCompleted * Bonus);
-            if (GameOver) {
-                System.out.println("Final Score is Equal to " + FinalScore);
-            }
+            CalculateScore(true,800,5,100);
 
-            Score = 1000;
-            LevelCompleted = 9;
-            Bonus = 210;
-
-            FinalScore = Score +(LevelCompleted * Bonus);
-            if(GameOver) {
-                System.out.println("Final Score is Equal to " + FinalScore);
-            }
+            CalculateScore(true,1000,9,210);
         }
 
-    public static void CalculateScore(boolean GameOver, int Score, int LevelCompleted, int Bonus){
-
+    public static void CalculateScore(boolean GameOver, int Score, int LevelCompleted, int Bonus) {
         int FinalScore = Score + (LevelCompleted * Bonus);
         if (GameOver == true) {
             System.out.println("Final Score is Equal to " + FinalScore);
         }
+
+    }
 }
+
